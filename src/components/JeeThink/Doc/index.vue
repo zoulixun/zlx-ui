87fe7566ec4a3bba10e2b30abdda0507e2fc6faf
zoulixun <13354038890@163.com>
2020-06-09 09:32:00 +0800

    zlx commit

---
+++ src/components/JeeThink/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="link" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zlxDoc',
+  data() {
+    return {
+      url: 'http://zlx.vip/zlxDoc'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/JeeThink/Site/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="guide" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zlxSite',
+  data() {
+    return {
+      url: 'http://zlx.vip'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
